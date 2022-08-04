2b6404ba6139655dbf2b9279d5e07e6968669824
banssa <ban8627@gmail.com>
2022-08-04 16:13:52 +0900

    헤더 작업 완료 비주얼 영역 시작

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
+  // header mouse enter event
+  let headerWrap = $('.header-wrap');
+  let headerHeight = 140;
+  let logo = $('.logo > a');
+  let gnb = $('.gnb');
+  let depth2 = $('.depth2');
+  let utillLang = $('.lang');
+  let siteMapbt = $('.site-map');
 
+  let headerActive = headerHeight + 250;
+  console.log(headerActive)
+  gnb.mouseenter(function(){
+    headerWrap.addClass('active').css('height',headerActive);
+    logo.addClass('active');
+    depth2.css('visibility','visible');
+    utillLang.addClass('active');
+    siteMapbt.addClass('active');
+  });
+  gnb.mouseleave(function(){
+    headerWrap.removeClass('active').css('height',headerHeight);
+    logo.removeClass('active');
+    depth2.css('visibility','hidden');
+    utillLang.removeClass('active');
+    siteMapbt.removeClass('active');
+  });
+
+  // header languge event
+  let languge = $(utillLang).find('li');
+  languge.click(function(event){
+    event.preventDefault();
+    languge.removeClass('active');
+    $(this).addClass('active');
+    });
 });
 
 
