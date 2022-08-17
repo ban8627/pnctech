0f928a4d9466cc38db5b46f85be60219b97ebd98
banssa <ban8627@gmail.com>
2022-08-17 18:08:01 +0900

    스크롤 이벤트 - 섹션 이동 작업 완료 섹션별 동작 작업중

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // header mouse enter event
   let headerWrap = $('.header-wrap');
   let headerHeight = 140;
@@ -9,55 +9,55 @@ $(document).ready(function(){
   let siteMapbt = $('.site-map');
 
   let headerActive = headerHeight + 250;
-  gnb.mouseenter(function(){
-    headerWrap.addClass('active').css('height',headerActive);
+  gnb.mouseenter(function () {
+    headerWrap.addClass('active').css('height', headerActive);
     logo.addClass('active');
-    depth2.css('visibility','visible');
+    depth2.css('visibility', 'visible');
     utillLang.addClass('active');
     siteMapbt.addClass('active');
   });
-  gnb.mouseleave(function(){
-    headerWrap.removeClass('active').css('height',headerHeight);
+  gnb.mouseleave(function () {
+    headerWrap.removeClass('active').css('height', headerHeight);
     logo.removeClass('active');
-    depth2.css('visibility','hidden');
+    depth2.css('visibility', 'hidden');
     utillLang.removeClass('active');
     siteMapbt.removeClass('active');
   });
 
   // header languge event
   let languge = $(utillLang).find('li');
-  languge.click(function(event){
+  languge.click(function (event) {
     event.preventDefault();
     languge.removeClass('active');
     $(this).addClass('active');
-    });
+  });
 
   // visual slide
-  let swVisual = new Swiper(".sw-visual",{
-    loop:true,
+  let swVisual = new Swiper(".sw-visual", {
+    loop: true,
     autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
+      delay: 3000,
+      disableOnInteraction: false,
     },
     pagination: {
-      el:'.sw-visual-bullet-box',
-      type:'bullets',
+      el: '.sw-visual-bullet-box',
+      type: 'bullets',
       clickable: true,
     },
     navigation: {
-        nextEl: ".sw-visual-next",
-        prevEl: ".sw-visual-prev",
+      nextEl: ".sw-visual-next",
+      prevEl: ".sw-visual-prev",
     },
-    on: {      
+    on: {
       slideChange: function () {
         let aBullet = this.realIndex + 1;
         let slideTotal = $('.sw-visual-bullet-box > span').length;
         let swFrac = $('.sw-visual-pgf');
-        if(slideTotal == 0){
+        if (slideTotal == 0) {
           slideTotal = 5;
-          swFrac.html( "<span class='sw-visual-pgf-current'>" + aBullet + "</span>" + '/' + slideTotal );
-        }else{
-          swFrac.html( "<span class='sw-visual-pgf-current'>" + aBullet + "</span>" + '/' + slideTotal );
+          swFrac.html("<span class='sw-visual-pgf-current'>" + aBullet + "</span>" + '/' + slideTotal);
+        } else {
+          swFrac.html("<span class='sw-visual-pgf-current'>" + aBullet + "</span>" + '/' + slideTotal);
         }
       }
     },
@@ -66,12 +66,12 @@ $(document).ready(function(){
   });
 
   let visualBt = $('.control-bt');
-  visualBt.click(function(){
+  visualBt.click(function () {
     let temp = visualBt.hasClass('pause');
-    if(temp == true){
+    if (temp == true) {
       swVisual.autoplay.stop();
       visualBt.removeClass('pause').addClass('play').text('play_arrow');
-    }else {
+    } else {
       swVisual.autoplay.start();
       visualBt.removeClass('play').addClass('pause').text('pause');
     }
@@ -82,45 +82,95 @@ $(document).ready(function(){
     $('.g-value').each(function () {
       let gvDC = $(this);
       let countTo = gvDC.attr('data-count');
-      $({countNum: (countTo - 100) < 0 ? 0 : (countTo - 100)}).animate({ 
-        countNum: countTo },
-        { duration: 1000,
-          easing: 'linear',
-          step: function () {
-            gvDC.text(Math.floor(this.countNum));
-          },
-          complete: function () {
-            gvDC.text(this.countNum);
-          }
+      $({
+        countNum: (countTo - 100) < 0 ? 0 : (countTo - 100)
+      }).animate({
+        countNum: countTo
+      }, {
+        duration: 1000,
+        easing: 'linear',
+        step: function () {
+          gvDC.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          gvDC.text(this.countNum);
         }
-      );
+      });
     });
   };
   valueCounter();
 
   // news-slide
-  let newsSwiper = new Swiper('.sw-news',{
+  let newsSwiper = new Swiper('.sw-news', {
     slidesPerView: 3,
     spaceBetween: 30,
-    navigation:{
+    navigation: {
       nextEl: ".news-next",
       prevEl: ".news-prev",
     },
-    pagination:{
-      el:'.news-fraction',
-      type:'fraction'
+    pagination: {
+      el: '.news-fraction',
+      type: 'fraction'
     }
   });
 
+  // left side bar bt
+  let leftSide = $('.l-side-bar');
+  let leftBtn = leftSide.find('a');
 
+  leftBtn.click(function () {
+    leftBtn.removeClass('on');
+    $(this).addClass('on');
+  })
 
   // footer - related box
   let fSite = $('.related-site');
-  fSite.click(function(){
+  fSite.click(function () {
     $(this).find('.related-list').toggle();
   })
 });
 
-window.onload = function(){
-  
-  };
+window.onload = function () {
+  AOS.init();
+
+  window.addEventListener("wheel", function (e) {
+    e.preventDefault();
+  }, {
+    passive: false
+  });
+  let pncHtml = $("html");
+  let page = 1;
+  let lSideBt = $('.l-side-bar').find('a');
+  let sideBar = $('.side-bar');
+
+  pncHtml.animate({
+    scrollTop: 0
+  }, 10);
+
+  $(window).on("wheel", function (e) {
+    if (pncHtml.is(":animated")) return;
+    if (e.originalEvent.deltaY > 0) {
+      lSideBt.removeClass('on');
+      lSideBt.eq(page).addClass('on');
+      if (page == 4) {
+        sideBar.hide();
+      }
+      if (page == 5) {
+        return;
+      }
+      page++;
+    } else if (e.originalEvent.deltaY < 0) {
+      lSideBt.removeClass('on');
+      lSideBt.eq(page - 2).addClass('on');
+      if (page == 5) {
+        sideBar.show();
+      }
+      if (page == 1) return;
+      page--;
+    }
+    let posTop = (page - 1) * $(window).height();
+    pncHtml.animate({
+      scrollTop: posTop
+    }, 800);
+  });
+};
